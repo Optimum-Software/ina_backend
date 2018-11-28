8aec3d9f059d065c19947b979e24c7205672bbd6
Wouter Kurvers <WKurvers11@gmail.com>
2018-11-28 12:20:40 +0100

    group class added

--- ina_api/models.py
+++ ina_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class User(models.Model):
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=100)
@@ -8,27 +9,37 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     salt = models.CharField(max_length=50, null=True)
     mobile = models.CharField(max_length=22)
-    organisation = models.CharField(max_length=100) #optional, example: Hanzehogeschool Groningen
-    function = models.CharField(max_length=100)		#optional, example: Docent Software Engineering
+    organisation = models.CharField(max_length=100)  # optional, example: Hanzehogeschool Groningen
+    function = models.CharField(max_length=100)  # optional, example: Docent Software Engineering
     profile_photo_path = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-    	return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_name
 
     def __repr__(self):
-    	return {
-    		"email": self.email,
-    		"firstName": self.first_name,
-    		"lastName": self.lastName,
-    		"bio": self.bio,
-    		"password": self.password,
-    		"salt": self.salt,
-    		"mobile": self.mobile,
-    		"organisation": self.organisation,
-    		"function": self.function,
-    		"profilePhotoPath": self.profile_photo_path
-    	}
+        return {
+            "email": self.email,
+            "firstName": self.first_name,
+            "lastName": self.lastName,
+            "bio": self.bio,
+            "password": self.password,
+            "salt": self.salt,
+            "mobile": self.mobile,
+            "organisation": self.organisation,
+            "function": self.function,
+            "profilePhotoPath": self.profile_photo_path
+        }
+
 
 class Project(models.Model):
-	name
+    name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    desc = models.TextField(max_length=2000)
+    photo_path = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now=True)
+    member_count = models.IntegerField(default=0)
+    public = models.BooleanField(default=True)
