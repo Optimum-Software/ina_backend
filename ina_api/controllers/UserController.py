9683d55867eeb1d07c18653427143c2184138e67
Bert de Boer <bertde.boer@hotmail.com>
2018-12-05 15:05:22 +0100

    login backand and frontend linked

--- ina_api/controllers/UserController.py
+++ ina_api/controllers/UserController.py
@@ -4,6 +4,9 @@ from django.db import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 from django.core import serializers
 import json
+
+from rest_framework.authtoken.models import Token
+
 from ina_api.models import *
 from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
@@ -22,6 +25,7 @@ def getUserById(request, id):
     except ObjectDoesNotExist:
         return JsonResponse({"bool": False, "msg": "User did not exist"}, safe=True)
 
+
 @api_view(['GET'])
 def test(request):
     try:
@@ -33,31 +37,41 @@ def test(request):
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-        
+
 
 class CreateUserAPIView(CreateAPIView):
     serializer_class = CreateUserSerializer
     permission_classes = [AllowAny]
+
     def create(self, request, *args, **kwargs):
         try:
             data = json.loads(request.body.decode('utf-8'))
-                
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+            try:
+                self.perform_create(serializer)
+            except IntegrityError as e:
+                return JsonResponse({"bool": False, "msg": "Account bestaat al"}, safe=True)
             headers = self.get_success_headers(serializer.data)
             # We create a token than will be used for future auth
+
             token = Token.objects.create(user=serializer.instance)
             token_data = {"token": token.key}
 
             try:
-                userObject = User(pk=serializer.instance.pk, email=data['username'], password=data['password'], first_name=data['firstName'],
-                                last_name=data['lastName'], bio=data['bio'], mobile=data['mobile'],
-                                organisation=data['organisation'], function=data['function'],
-                                profile_photo_path=data['profilePhotoPath'])
+                userObject = User(pk=serializer.instance.pk,
+                                  email=data['username'],
+                                  password=data['password'],
+                                  first_name=data['firstName'],
+                                  last_name=data['lastName'],
+                                  bio=data['bio'],
+                                  mobile=data['mobile'],
+                                  organisation=data['organisation'],
+                                  function=data['function'],
+                                  profile_photo_path=data['profilePhotoPath'])
                 userObject.save()
 
-                return JsonResponse({"bool": True, "msg": "User entry created", "id": userObject.pk,}, safe=True)
+                return JsonResponse({"bool": True, "msg": "User entry created", "id": userObject.pk, }, safe=True)
             except:
                 return JsonResponse({"bool": False, "msg": "Could not create user"}, safe=True)
         except IntegrityError as e:
