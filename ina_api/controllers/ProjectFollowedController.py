abe0a4a4582a3c014ffa9ac8db18d309cd592bc3
Wouter Kurvers <WKurvers11@gmail.com>
2018-12-20 15:58:16 +0100

    followProjectById added

--- ina_api/controllers/ProjectFollowedController.py
+++ ina_api/controllers/ProjectFollowedController.py
@@ -17,3 +17,18 @@ def getProjectFollowedById(request, id):
         return JsonResponse({"bool": True, "msg": "ProjectFollowed bestaat", "projectFollowed": projectFollowedJson, "user": userObject, "project": projectObject}, safe=True)
     except ObjectDoesNotExist:
         return JsonResponse({"bool": False, "msg": "ProjectFollowed bestaat niet"}, safe=True)
+
+@require_http_methods(['POST'])
+def followProjectById(request):
+    data = json.loads(request.body.decode('utf8'))
+    projectId = data['id']
+    userId = data['userId']
+
+    try:
+        projectObject = Project.objects.get(pk=projectId)
+        userObject = Project.objects.get(pk=userId)
+        projectFollowed = Project_Followed(project=projectObject.name, user=userObject)
+        projectFollowed.save()
+        return JsonResponse({"bool": True, "msg": "Je volgt nu het project"}, safe=True)
+    except:
+        return JsonResponse({"bool": False, "msg": "volgen is mislukt"}, safe=True)
