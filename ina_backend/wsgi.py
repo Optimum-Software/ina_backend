e0df05e6688bbf8ad121fed695ba4009cebae7d9
unknown <jelmer.haarman@xs4all.nl>
2019-01-23 12:48:13 +0100

    updates

--- ina_backend/wsgi.py
+++ ina_backend/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys, django
 from django.core.wsgi import get_wsgi_application
 
+print(django.__file__)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ina_backend.settings")
 
 application = get_wsgi_application()
